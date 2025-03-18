1f848c27aad010db445735772611d2c05c9146d6
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-18 21:16:38 +0600

    Roadmap Prompt Engineering #13 : maybe ok...?

--- api/controllers/roadmap.controller.js
+++ api/controllers/roadmap.controller.js
@@ -24,7 +24,7 @@ export const generateRoadmap = async (req, res) => {
     const completion = await groq.chat.completions.create({
       messages: [{ role: "user", content: prompt }],
       model: "llama-3.3-70b-versatile",
-      temperature: 0.3,
+      temperature: 0.9,
     });
 
     const roadmapContent = completion.choices[0].message.content;
